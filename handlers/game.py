d32e5f19064c92b3400f29364f83fdd1febd3133
Kuznetsova Lera <kuzlera@mail.ru>
2022-04-28 21:44:32 +0300

    Игра работает на сколь угодно вопросов красиво.

--- handlers/game.py
+++ handlers/game.py
@@ -7,12 +7,9 @@ from create_bot import dp, bot
 
 
 class Game(StatesGroup):
-    count = 0
+    count_answer = 0
+    count_questions = 1
     q1 = State()
-    q2 = State()
-    q3 = State()
-    q4 = State()
-    q5 = State()
 
 
 async def cmd_game(message: types.Message):
@@ -22,72 +19,26 @@ async def cmd_game(message: types.Message):
     await Game.q1.set()
 
 
-async def answer_q1(message: types.Message, state: FSMContext):
+async def answer(message: types.Message, state: FSMContext):
+    Game.count_questions += 1
     await state.update_data(answer1=message.text)
     data = await state.get_data()
     answer1 = data.get('answer1')
     if answer1 == true_answer:
-        Game.count += 1
+        Game.count_answer += 1
     generate_markup()
     await bot.send_photo(chat_id=message.chat.id, photo=raw[1])
     await message.answer("Выберите правильный ответ", reply_markup=markup)
-    await Game.next()
-
-
-async def answer_q2(message: types.Message, state: FSMContext):
-    await state.update_data(answer1=message.text)
-    data = await state.get_data()
-    answer1 = data.get('answer1')
-    if answer1 == true_answer:
-        Game.count += 1
-    generate_markup()
-    await bot.send_photo(chat_id=message.chat.id, photo=raw[1])
-    await message.answer("Выберите правильный ответ", reply_markup=markup)
-    await Game.next()
-
-
-async def answer_q3(message: types.Message, state: FSMContext):
-    await state.update_data(answer1=message.text)
-    data = await state.get_data()
-    answer1 = data.get('answer1')
-    if answer1 == true_answer:
-        Game.count += 1
-    generate_markup()
-    await bot.send_photo(chat_id=message.chat.id, photo=raw[1])
-    await message.answer("Выберите правильный ответ", reply_markup=markup)
-    await Game.next()
-
-
-async def answer_q4(message: types.Message, state: FSMContext):
-    await state.update_data(answer1=message.text)
-    data = await state.get_data()
-    answer1 = data.get('answer1')
-    if answer1 == true_answer:
-        Game.count += 1
-    generate_markup()
-    await bot.send_photo(chat_id=message.chat.id, photo=raw[1])
-    await message.answer("Выберите правильный ответ", reply_markup=markup)
-    await Game.next()
-
-
-async def answer_q5(message: types.Message, state: FSMContext):
-    await state.update_data(answer1=message.text)
-    data = await state.get_data()
-    answer1 = data.get('answer1')
-    if answer1 == true_answer:
-        Game.count += 1
-    print(Game.count)
-    await message.answer("Вы ответили верно на " + str(Game.count) + " вопросов.")
-    await state.finish()
+    if Game.count_questions < 5:
+        await Game.q1.set()
+    else:
+        await state.finish()
+        print(Game.count_answer)
 
 
 def register_handlers_game(dp: Dispatcher):
     dp.register_message_handler(cmd_game, commands=['game'])
-    dp.register_message_handler(answer_q1, state=Game.q1)
-    dp.register_message_handler(answer_q2, state=Game.q2)
-    dp.register_message_handler(answer_q3, state=Game.q3)
-    dp.register_message_handler(answer_q4, state=Game.q4)
-    dp.register_message_handler(answer_q5, state=Game.q5)
+    dp.register_message_handler(answer, state=Game.q1)
 
 
 def generate_markup():
